f6a4a825cf0f7912f24ead618b5bcfeee22c63eb
maerwen <2353200015@qq.com>
2018-04-18 11:36:20 +0800

    初始版本

---
+++ 5.Function/5.3ReturnValues/multiple.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+
+// 多返回值函数
+func test() (value string, ok bool) {
+	return //同 return "",false
+	// kvs := make(map[string]string)
+	// value, ok = kvs["key"]
+	// return // 同于 return value,ok
+}
