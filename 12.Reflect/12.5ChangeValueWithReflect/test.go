afcb566e7333c94f1ea2ff2388c1556978180b2e
wangzhaoxiang <wangzhaoxiang@huania.com>
2018-05-30 17:50:17 +0800

    change value with reflect

--- 12.Reflect/12.5ChangeValueWithReflect/test.go
+++ 12.Reflect/12.5ChangeValueWithReflect/test.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	test()
+}
+func test() {
+	x := 2
+	a := reflect.ValueOf(2)
+	b := reflect.ValueOf(x)
+	c := reflect.ValueOf(&x)
+	d := c.Elem()
+	fmt.Println(a.CanAddr()) //false
+	fmt.Println(b.CanAddr()) //false
+	fmt.Println(c.CanAddr()) //false
+	fmt.Println(d.CanAddr()) //true
 }
